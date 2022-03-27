0d69f91443fa6732d2a5d45425298f620e807d4a
uforgot <uforgot@designfever.com>
2022-03-27 11:25:20 +0900

    dynamic route add

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,66 @@
 import Head from 'next/head'
 import Layout, {siteTitle} from "./components/layout";
 import utilStyles from "../styles/utils.module.scss"
+import {getSortedPostsData} from "../lib/posts";
 
-export default function Home() {
-  return (
-      <Layout home>
-        <Head>
-          <title>{siteTitle}</title>
-        </Head>
-        <section className={utilStyles.headingMd}>
-            <p>[hello !!]</p>
-            <p>
-                this is sample website!!!
-                <a href="https://designfever.com" target="_blank">designfever.com</a>
-            </p>
-        </section>
-      </Layout>
-  )
+import Link from 'next/link'
+import Date from "./components/date";
+
+// export default function Home() {
+//   return (
+//       <Layout home>
+//         <Head>
+//           <title>{siteTitle}</title>
+//         </Head>
+//         <section className={utilStyles.headingMd}>
+//             <p>[hello !!]</p>
+//             <p>
+//                 this is sample website!!!
+//                 <a href="https://designfever.com" target="_blank">designfever.com</a>
+//             </p>
+//         </section>
+//       </Layout>
+//   )
+// }
+
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
+
+export default function Home({ allPostsData }) {
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <section className={utilStyles.headingMd}>
+                <p>[hello !!]</p>
+                <p>
+                    this is sample website!!!
+                    <a href="https://designfever.com" target="_blank">designfever.com</a>
+                </p>
+            </section>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({id,date,title})=>(
+                      <li className={utilStyles.listItem} key={id}>
+                          <Link href={`/posts/${id}`}>
+                            <a>{title}</a>
+                          </Link>
+                          <br/>
+                          <small className={utilStyles.lightText}>
+                              <Date dateString={date}/>
+                          </small>
+                      </li>
+                    ))}
+                </ul>
+            </section>
+        </Layout>
+    )
 }
